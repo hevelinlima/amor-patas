7fd4f33759ca1d93027ecd451f227418f26d0a15
hevelinlima <hevelin25@gmail.com>
2023-11-15 17:28:32 -0300

    verificações de cadastro

--- venv/usuarios/views.py
+++ venv/usuarios/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Usuario
+from django.urls import reverse 
+from hashlib import sha256
 
 # Create your views here.
 def inicio(request):
@@ -13,7 +15,8 @@ def login(request):
   return HttpResponse('login')
 
 def cadastro(request):
-  return render(request,'cadastro.html')
+  status = request.GET.get('status')
+  return render(request,'cadastro.html', {'status': status})
 
 def validacao(request):
   nome = request.POST.get('nome')
@@ -21,7 +24,23 @@ def validacao(request):
   email = request.POST.get('email')
   senha = request.POST.get('senha')
 
-  usuario = Usuario(nome=nome, sobrenome=sobrenome, email=email, senha=senha)
-  usuario.save()
+  usuario = Usuario.objects.filter(email = email)
+  if len(nome.strip()) == 0 or len(email.strip()) == 0 or len(sobrenome.strip()) == 0:
+    return redirect(reverse('usuarios:cadastro') + '?status=1')
+  
+  if len(senha) < 8:
+    return redirect(reverse('usuarios:cadastro') + '?status=2')
 
-  return HttpResponse(f'{nome} {sobrenome}, {email}, {senha}')
+  if len(usuario) > 0:
+    return redirect(reverse('usuarios:cadastro') + '?status=3')
+  try:
+    senha = sha256(senha.encode()).hexdigest()
+    usuario = Usuario(nome=nome, sobrenome=sobrenome, email=email, senha=senha)
+    usuario.save()
+
+    return redirect(reverse('usuarios:cadastro') + '?status=0')
+  except:
+    return redirect(reverse('usuarios:cadastro') + '?status=4')
+  
+
+  
